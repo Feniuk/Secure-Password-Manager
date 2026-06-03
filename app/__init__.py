ca565cb6f0f952b2fdaaff836bf0ff1aea2be776
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-06-03 23:55:13 +0200

    error fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,10 @@ myDB = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config["SECRET_KEY"] = "dev-key"
+    app.config["SECRET_KEY"] = "my_secret_key "
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///password_manager.db"
-
     myDB.init_app(app)
-
+    @app.route("/")
+    def landing_page():
+        return "Secure Password Manager"
     return app
